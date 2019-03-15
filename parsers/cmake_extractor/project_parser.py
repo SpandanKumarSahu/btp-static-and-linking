77f2c7e4cf556799a6e43f58bc2e4368ab42b99b
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2019-03-15 05:36:57 +0530

    improve project_parser, with documentation
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- parsers/cmake_extractor/project_parser.py
+++ parsers/cmake_extractor/project_parser.py
@@ -1,14 +1,43 @@
 import re
 import sys
+import pprint
 
+# The compiler used by the project. In this example, it would `/usr/bin/cc `
+# because it is a C-based project. Assuming default is C++. Can be changed by user
 CXX_COMPILER = "/usr/bin/c++ "
+
+#[TODO]: Implement support for other archivers (NOT A PRIORITY)
 STATIC_ARCHIVER = "/usr/bin/ar "
+
 CHANGE_DIRECTORY_CMD = "cd "
 
+# filename: The make_log.txt file, which contains the verbose output of the build
+# We try to extract various dependencies using this file
 filename = sys.argv[1]
+
+# for C++ projects, it would be "/usr/bin/c++ "
+# for C projects, it would be "/usr/bin/cc "
+#[TODO]: Support multiple number of compilers, for projects that are multi-lingual
+if len(sys.argv) > 2:
+    CXX_COMPILER = str(sys.argv[2]) + str(" ")
+
+
 with open(filename, "r") as f:
     data = f.readlines()
+'''
+We open the make_log.txt file and try to find 3 types of essential commands:
+1. CXX_COMMANDS: These commands including compiling and linking instructions
+2. CD_COMMANDS: We use these commands to keep track of the directory of the files
+                we are compiling/linking
+3. AR_COMMANDS: Archiving commands. NOT RELEVANT NOW. But this command helps to
+                code structure, and if there is a library (internal) being used.
+                Might become useful in future
 
+The algorithm is pretty simple. The way CMake generates the Makefile and the way
+Makefile builds objects and binaries, is very standard, making the parsing
+straightforward. We are just looking for the occurance of the three commands
+mentioned above, and store relevant information in lists.
+'''
 cxx_cmds = []
 cd_cmds = []
 static_link_cmds = []
@@ -27,6 +56,24 @@ for line_num, x in enumerate(data):
     elif STATIC_ARCHIVER in x:
         static_link_cmds.append((line_num, x))
 
+for i in range(3):
+    print("\t")
+
+# For this small project, it is sufficient to print this
+for line_num, path, data in cxx_cmds:
+    print(data)
+
+for i in range(3):
+    print("\t")
+
+# The tree representation of the very same information as printed above. It has
+# relevance for big projects like symengine and if we want to parse external
+# or standard libraries also
+print("This is the TREE REPRESENTATION of the very same information.")
+
+for i in range(3):
+    print("\t")
+
 tree = {}
 tree['.'] = []
 
@@ -40,5 +87,9 @@ for line_num, path, data in cxx_cmds:
             temp_dict[x] = {}
             temp_dict = temp_dict[x]
             temp_dict['.'] = []
+        else:
+            temp_dict = temp_dict[x]
     temp_dict['.'].append(data)
-print(tree)
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(tree)
